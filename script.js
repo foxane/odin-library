ea9e8b12135c62aec74e6453bbf417d4385d3031
foxane <65512187+foxane@users.noreply.github.com>
2024-05-06 23:43:55 +0700

    Added modal

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const myLibrary = [];
 function Book() {}
 
 function addBookToLibrary() {}
+
+const addBtn = document.querySelector('.btn-add');
+const dialog = document.querySelector('.dialog');
+
+addBtn.addEventListener('click', function () {
+  dialog.showModal();
+});
