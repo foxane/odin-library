687ee7c07922b1451403cff59f60934b22213653
foxane <65512187+foxane@users.noreply.github.com>
2024-05-07 19:14:34 +0700

    Added input validation

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const libraryUi = document.querySelector('.library');
 const btnAdd = document.querySelector('.btn-add');
 // | Modal
 const modal = document.querySelector('dialog');
-
+const form = document.querySelector('form');
+const inputNodes = document.querySelectorAll('input');
 const btnSubmit = document.querySelector('.btn-submit');
 const btnCancel = document.querySelector('.btn-cancel');
 
@@ -25,6 +26,16 @@ function Book(
 }
 
 // Input control
+// fuck you e,+,-
+const invalidChars = ['-', '+', 'e'];
+inputNodes.forEach((el) => {
+  if (el.getAttribute('type') === 'number') {
+    el.addEventListener('keydown', (e) => {
+      if (invalidChars.includes(e.key)) e.preventDefault();
+    });
+  }
+});
+// prevent input retain their value after submission
 function clearInput() {
   document.querySelectorAll('input').forEach((element) => {
     if (element.getAttribute('type') !== 'checkbox') {
@@ -40,8 +51,6 @@ function addBookToLibrary(title, author, pages, year, read) {
   console.log(library);
 }
 
-// UI control
-
 // Button control
 btnAdd.addEventListener('click', function () {
   clearInput();
@@ -51,4 +60,21 @@ btnCancel.addEventListener('click', function () {
   modal.close();
   clearInput();
 });
-btnSubmit.addEventListener('click', function () {});
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const cleanInput = [];
+  inputNodes.forEach((el) => {
+    if (el.getAttribute('type') !== 'checkbox') {
+      cleanInput.push(el.value ? el.value : 'Unknown');
+    } else {
+      cleanInput.push(el.checked);
+    }
+  });
+
+  addBookToLibrary(...cleanInput);
+  this.submit();
+});
+
+// UI control
+//oh boy..
